5c56941862a7b5cad0493f602a1ae370e88d901c
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2022-11-03 22:42:19 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -26,11 +26,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
+// app.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
